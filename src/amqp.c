7db8343bc326630a195e7a7fd6da09a5ec485070
erso <edusillva784@gmail.com>
2023-09-21 20:05:07 -0300

    Added core methods to amqp.c.
    It will be necessary to create a state machine to handle sending
    messages, but I do not want to make this project too much of a freak,
    having many files, so I will make it happen inside amqp.c. Kinda lazy
    tbh.

---
+++ src/amqp.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "amqp.h"
+#include "hardcode.h"
+#include <string.h>
+
+int parse_frame_type(char * recvline){
+    return recvline[0];
+}
+
+char* create_connection_start_ok_packet(){
+    char* pkt = NULL;
+    memcpy(pkt,CONNECTION_START_OK_PKT, strlen(CONNECTION_START_OK_PKT));
+    return pkt; 
+}
+
+char* create_connection_tune_ok_packet(){
+    char* pkt = NULL;
+    memcpy(pkt,CONNECTION_TUNE_OK_PKT, strlen(CONNECTION_TUNE_OK_PKT));
+    return pkt; 
+}
+
+char* create_connection_open_ok_packet(){
+    char* pkt = NULL;
+    memcpy(pkt,CONNECTION_OPEN_OK_PKT, strlen(CONNECTION_OPEN_OK_PKT));
+    return pkt; 
+}
+
+char* create_connection_close_ok_packet(){
+    char* pkt = NULL;
+    memcpy(pkt,CONNECTION_CLOSE_OK_PKT, strlen(CONNECTION_CLOSE_OK_PKT));
+    return pkt; 
+}
+
+char* create_channel_open_ok_packet(){
+    char* pkt = NULL;
+    memcpy(pkt,CHANNEL_OPEN_OK_PKT, strlen(CHANNEL_OPEN_OK_PKT));
+    return pkt; 
+}
+
+char* create_channel_close_ok_packet(){
+    char* pkt = NULL;
+    memcpy(pkt,CHANNEL_OPEN_OK_PKT, strlen(CHANNEL_OPEN_OK_PKT));
+    return pkt; 
+}
+
+char* create_queue_declare_ok_packet(){
+    char* pkt = NULL;
+    memcpy(pkt,QUEUE_DECLARE_OK_PKT, strlen(QUEUE_DECLARE_OK_PKT));
+    return pkt; 
+}
+
+char* create_basic_consume_ok_packet(){
+    char* pkt = NULL;
+    memcpy(pkt,BASIC_CONSUME_OK_PKT, strlen(BASIC_CONSUME_OK_PKT));
+    return pkt; 
+}
+
+char* create_basic_qos_ok_packet(){
+    char* pkt = NULL;
+    memcpy(pkt,BASIC_QOS_OK_PKT, strlen(BASIC_QOS_OK_PKT));
+    return pkt; 
+}
+
+char* create_basic_qos_ok_packet(){
+    char* pkt = NULL;
+    memcpy(pkt,BASIC_QOS_OK_PKT, strlen(BASIC_QOS_OK_PKT));
+    return pkt; 
+}
