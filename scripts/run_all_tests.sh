d8b994053148464bb22152ddc536f5d30f7dbcc5
erso <edusillva784@gmail.com>
2023-10-09 10:08:57 -0300

    Fixed wrong data

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 SAMPLE_SIZE=100
-MESSAGES=30
+MESSAGES=1000
 docker start ep1;
 echo "Started docker container";
 sleep 5;
 ./declare-queue.sh $1;
-./publish.sh $1 $MESSAGES & 
-./consume.sh $1 1 &
+./publish.sh 1 $MESSAGES & 
+./consume.sh 1 $1 &
 echo "Started get_cpu_network_from_docker"
 ./get_cpu_network_from_docker.sh $SAMPLE_SIZE $1
 docker kill ep1;
